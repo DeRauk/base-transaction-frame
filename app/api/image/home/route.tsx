f49bb15250991ac33061ddded5b3766078393e62
DeRauk Gibble <derauk.gibble@coinbase.com>
2024-02-28 09:14:11 -0500

    Fix

--- app/api/image/home/route.tsx
+++ app/api/image/home/route.tsx
@@ -1,31 +1,31 @@
-import { ImageResponse } from '@vercel/og';
-
 export const runtime = 'edge';
 
 export async function GET() {
-    return new ImageResponse(
-      (
-        <div
-          style={{
-            fontSize: 40,
-            color: 'black',
-            background: 'white',
-            width: '100%',
-            height: '100%',
-            padding: '50px 200px',
-            textAlign: 'center',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          See how many base transactions you have!
-        </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-      },
-    );
-  }
+  const { ImageResponse } = await import('@vercel/og');
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        See how many base transactions you have!
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
 
 export const dynamic = 'force-dynamic';
